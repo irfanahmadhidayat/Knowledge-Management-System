9bae53411718ae707ff664be4ce0006115bba1ff
irfanahmadhidayat <hidayatirfanahmad7@gmail.com>
2025-03-12 22:04:35 +0700

    integrasi dashboard admin, controller Category, Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::resource('dashboard', DashboardController::class);
+Route::resource('category', CategoryController::class);
